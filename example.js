758bef74a5ff83764fdcf41b1be7e00bb6e63000
jspenc72 <jspenc72@gmail.com>
2016-01-14 15:23:45 -0700

    Updated example.

--- example.js
+++ example.js
@@ -1,6 +1,7 @@
 var username = "6228032fc25b59e3363e75bacc6a3960"
 var password = "2d430177512f8414445162122a70c921"
 var intrinio = require("./index.js")(username, password)
+
 intrinio
 .ticker('AAPL')
 .on('complete', function(data, response) {
@@ -9,4 +10,5 @@ intrinio
 	}else if(response.statusCode==200){
 		console.log("Everything is OK!")
 	}
+	console.log(data)
 });
