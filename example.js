8dadae93ecd106ddc031f4fbe929c0f6638cefd9
jspenc72 <jspenc72@gmail.com>
2016-01-14 15:52:04 -0700

    Update example to show additional endpoints.

--- example.js
+++ example.js
@@ -1,14 +1,106 @@
-var username = "6228032fc25b59e3363e75bacc6a3960"
-var password = "2d430177512f8414445162122a70c921"
+var username = "" //Your Username
+var password = "" //Your Password
 var intrinio = require("./index.js")(username, password)
 
 intrinio
 .ticker('AAPL')
 .on('complete', function(data, response) {
 	if(response.statusCode==404){
-		console.log("Not found")
+		console.log("Not found ticker")
 	}else if(response.statusCode==200){
-		console.log("Everything is OK!")
+		console.log("Everything is OK with .ticker!")
+	}
+	console.log(data)
+});
+
+intrinio
+.prices('AAPL')
+.on('complete', function(data, response) {
+	if(response.statusCode==404){
+		console.log("Not found prices")
+	}else if(response.statusCode==200){
+		console.log("Everything is OK with .prices!")
+	}
+	console.log(data)
+});
+
+
+intrinio
+.historical_data('AAPL')
+.on('complete', function(data, response) {
+	if(response.statusCode==404){
+		console.log("Not found historical_data")
+	}else if(response.statusCode==200){
+		console.log("Everything is OK with .historical_data!")
+	}
+	console.log(data)
+});
+
+
+intrinio
+.companies('AAPL')
+.on('complete', function(data, response) {
+	if(response.statusCode==404){
+		console.log("Not found companies")
+	}else if(response.statusCode==200){
+		console.log("Everything is OK with .companies!")
+	}
+	console.log(data)
+});
+
+
+intrinio
+.securities('AAPL')
+.on('complete', function(data, response) {
+	if(response.statusCode==404){
+		console.log("Not found securities")
+	}else if(response.statusCode==200){
+		console.log("Everything is OK with .securities!")
+	}
+	console.log(data)
+});
+
+intrinio
+.indices('AAPL')
+.on('complete', function(data, response) {
+	if(response.statusCode==404){
+		console.log("Not found indices")
+	}else if(response.statusCode==200){
+		console.log("Everything is OK with .indices!")
+	}
+	console.log(data)
+});
+
+
+intrinio
+.data_point('AAPL')
+.on('complete', function(data, response) {
+	if(response.statusCode==404){
+		console.log("Not found data_point")
+	}else if(response.statusCode==200){
+		console.log("Everything is OK with .data_point!")
+	}
+	console.log(data)
+});
+
+intrinio
+.historical_data('AAPL')
+.on('complete', function(data, response) {
+	if(response.statusCode==404){
+		console.log("Not found historical_data")
+	}else if(response.statusCode==200){
+		console.log("Everything is OK with .historical_data!")
+	}
+	console.log(data)
+});
+
+intrinio
+.news('AAPL')
+.on('complete', function(data, response) {
+	if(response.statusCode==404){
+		console.log("Not found news")
+	}else if(response.statusCode==200){
+		console.log("Everything is OK with .news!")
 	}
 	console.log(data)
 });
--- index.js
+++ index.js
@@ -9,7 +9,38 @@ module.exports = function (username, password) {
 	  baseURL: 'https://www.intrinio.com/api'
 	}, {
 	  ticker: function(ticker) {
+//
 	    return this.get('https://www.intrinio.com/api/companies?ticker='+ticker);
+	  },
+	  prices: function(ticker) {
+// 
+	    return this.get('https://www.intrinio.com/api/prices?ticker='+ticker);
+	  },
+	  historical_data: function(ticker) {
+	    return this.get('https://www.intrinio.com/api/historical_data?ticker='+ticker);
+	  },
+	  companies: function(ticker) {
+// 
+	    return this.get('https://www.intrinio.com/api/companies?ticker='+ticker);
+	  },
+	  securities: function(ticker) {
+// 
+	    return this.get('https://www.intrinio.com/api/securities?ticker='+ticker);
+	  },
+	  indices: function(ticker) {
+// Is returning an empty object.
+	    return this.get('https://www.intrinio.com/api/indices?ticker='+ticker);
+	  },
+	  data_point: function(ticker, item) {
+//
+	    return this.get('https://www.intrinio.com/api/data_point?ticker='+ticker+"&item="+item);
+	  },
+	  historical_data: function(ticker, item) {
+	    return this.get('https://www.intrinio.com/api/historical_data?ticker='+ticker+"&item="+item);
+	  },
+	  news: function(ticker) {	    
+// 
+	    return this.get('https://www.intrinio.com/api/news?ticker='+ticker);
 	  }
 	});
 	if(!username || !password){
